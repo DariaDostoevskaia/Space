9224773439cd0fca47d17a5e40ef8d8125ad5f69
DariaDostoevskaia <dar.dost@mail.ru>
2023-03-25 19:09:11 +0300

    feat: movement ( )

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -14,16 +14,29 @@ public class PlayerShip : MonoBehaviour
     private float _currentSpeed;
     private Vector3 _lastMovement;
     private Vector3 _mousePosition;
+    public SpriteRenderer sprite;
+    public Animator charAnimator;
 
     private void Start()
     {
-        
+        //_items
     }
 
     private void Update()
     {
         _mousePosition = Input.mousePosition;
         _mousePosition = Camera.main.ScreenToWorldPoint(_mousePosition);
+
+        if (Input.GetButton("Horizontal"))
+        {
+            Movement();
+            charAnimator.SetInteger("State", 1);
+        }
+        else
+        {
+            charAnimator.SetInteger("State", 0);
+        }
+        
     }
     private void FixedUpdate()
     {
@@ -33,7 +46,16 @@ public class PlayerShip : MonoBehaviour
 
     private void Movement()
     {
-       
+        Vector3 tempVector = Vector3.right * Input.GetAxis("Horizontal");
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + tempVector, _currentSpeed*Time.deltaTime);
+        if (tempVector.x < 0)
+        {
+            sprite.flipX = true;
+        }
+        else 
+        {
+            sprite.flipX = false;
+        }
     }
 
     private void Rotation()
