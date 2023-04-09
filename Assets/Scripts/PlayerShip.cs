672cf091b32d0465cee8b6707270dc0ab9e0bc2a
DariaDostoevskaia <dar.dost@mail.ru>
2023-04-10 00:00:30 +0300

    feat: laser

--- Assets/Scripts/PlayerShip.cs
+++ Assets/Scripts/PlayerShip.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,11 +7,18 @@ public abstract class PlayerShip : MonoBehaviour
     [SerializeField] private float _playerSpeed = 2f;
     [SerializeField] private float _inertia = 0.9f;
     [SerializeField] private bool _movementBowShip;
+
     [SerializeField] private List<KeyCode> _upButtons;
     [SerializeField] private List<KeyCode> _downButtons;
     [SerializeField] private List<KeyCode> _leftButtons;
     [SerializeField] private List<KeyCode> _rightButtons;
 
+    [SerializeField] private Transform _laser;
+    [SerializeField] private float _laserDistance;
+    [SerializeField] private float _timeBetweenFires;
+    [SerializeField] private List<KeyCode> _shootButtons;
+
+    private float _timeNextFire;
     private float _currentSpeed;
     private Vector3 _lastMovement;
     private Vector3 _movement;
@@ -33,6 +41,28 @@ public abstract class PlayerShip : MonoBehaviour
         _movement.Normalize();
 
         HandleTargetRotation();
+
+        foreach (var element in _shootButtons)
+        {
+            if (Input.GetKey(element)
+                && _timeNextFire < 0)
+            {
+                _timeNextFire = _timeBetweenFires;
+                ShootLaser();
+            }
+        }
+        _timeNextFire -= Time.deltaTime;
+    }
+
+    private void ShootLaser()
+    {
+        var positionX = transform.position.x;
+        //+(Mathf.Cos(transform.localEulerAngles.z - 90 + Mathf.Deg2Rad) * -_laserDistance);
+        var positionY = transform.position.y;
+        //+(Mathf.Cos(transform.localEulerAngles.z - 90 + Mathf.Deg2Rad) * -_laserDistance);
+        //требуется рефакторинг - создай отдельный метод
+
+        Instantiate(_laser, new Vector3(positionX, positionY, 0), transform.rotation);
     }
 
     private void FixedUpdate()
--- Assets/Scripts/Sample.cs
+++ Assets/Scripts/Sample.cs
@@ -9,21 +9,6 @@ public class Sample : MonoBehaviour
 
     private void Start()
     {
-        var items = new List<string>();
-        for (int i = 1; i <= 10; i++)
-        {
-            items.Add($"item_{i}");
-        }
-        for (int i = 0; i < items.Count; i++)
-        {
-            Debug.Log(items[i]);
-        }
-
-        var positions = new List<Vector3>(20);
-        var number = Random.Range(0, 10);
-        for (int i = 0; i < 20; i++)
-        {
-        }
     }
 
     private void Update()
