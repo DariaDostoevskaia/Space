1cefa0674c1da0346ab665561743e6e2fcfce043
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2023-05-08 20:47:11 +0300

    fix: cosinus vector

--- Assets/Scripts/с#/Cosinus4.cs
+++ Assets/Scripts/с#/Cosinus4.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class Cosinus4 : MonoBehaviour
 {
     [SerializeField] private float _cosinus;
+    private Vector3 _point;
 
     private void Update()
     {
+        Vector3 point = transform.position;
         if (Input.GetMouseButtonDown(0))
         {
-            _cosinus = Mathf.Cos(_cosinus);
+            _cosinus = Mathf.Cos(_point.x);
             Debug.Log(_cosinus);
         }
     }
