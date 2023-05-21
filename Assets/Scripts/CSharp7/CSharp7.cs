9a0e531c81548b65d999f889ab7024713523f955
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2023-05-21 22:53:10 +0300

    fix: list select

--- Assets/Scripts/CSharp7/CSharp7.cs
+++ Assets/Scripts/CSharp7/CSharp7.cs
@@ -1,35 +1,16 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class CSharp7 : MonoBehaviour
 {
-    private void Start()
+    public List<int> GetXPositions(List<Vector3> vectors)
     {
-        //List<int> numQuery2 = (from num in numbers where (num % 2) == 0 select num)
-        //    .ToList();
-        var xPositions = new List<int>();
-        var list = Enumerable.Range(1, 10)
-            .Select(i => i * i)
-            .ToList();
+        var xPositions = vectors
+           .Where(vector => vector.x > 5)
+           .Select(vector => (int)vector.x)
+           .ToList();
+        return xPositions;
     }
-
-    //public List<int> GetXPositions(List<Vector3> vectors)
-    //{
-    //var xPositions = new List<int>();
-    //for (int i = 0; i < vectors.Length; i++)
-    //{
-    //    if (vectors[i].x > 5)
-    //        xPositions.Add(letters[i].x);
-    //}
-    //return xPositions;
-    //}
-
-    //public IEnumerable<int> GetNewLetterIds_LinqWay(IEnumerable<object> letters)
-    //{
-    //    return letters
-    //        .Where(letter => letter
-    //        .IsNew)
-    //        .Select(letter => letter.Id);
-    //}
 }
