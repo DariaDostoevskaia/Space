eedb843b2db84954475f9b7c7f3e0d54e95d6028
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2023-05-21 23:03:43 +0300

    fix: try get component result

--- Assets/Scripts/с#/Exercise7/Game7Object1.cs
+++ Assets/Scripts/с#/Exercise7/Game7Object1.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class Game7Object1 : MonoBehaviour
 {
-    [SerializeField] private Space5OnOff _space5OnOff;
-    //private GameObject _gameObject;
+    [SerializeField] private GameObject _gameObject;
+    private Space5OnOff _space5OnOff;
+
+    private void Start()
+    {
+        if (!_gameObject.TryGetComponent<Space5OnOff>(out _space5OnOff))
+            Debug.LogError("Component missing.");
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            //_gameObject.TryGetComponent(out _space5OnOff);
             _space5OnOff.enabled = !_space5OnOff.enabled;
         }
     }
