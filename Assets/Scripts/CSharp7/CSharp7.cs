0e4e1588d79f84f040051e9331a02fe4be3d12f7
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2023-05-23 00:47:42 +0300

    feat: exercices 2, 3, 4

--- Assets/Scripts/CSharp7/CSharp7.cs
+++ Assets/Scripts/CSharp7/CSharp7.cs
@@ -1,10 +1,12 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class CSharp7 : MonoBehaviour
 {
+    [SerializeField] private int _skipCount;
+    [SerializeField] private int _takeCount;
+
     public List<int> GetXPositions(List<Vector3> vectors)
     {
         var xPositions = vectors
@@ -12,5 +14,27 @@ public class CSharp7 : MonoBehaviour
            .Select(vector => (int)vector.x)
            .ToList();
         return xPositions;
+
+        void GetArray()
+        {
+            // Exercise 3.Skip, ToArray
+            var array = xPositions.ToArray();
+
+            if (_skipCount >= 0
+                && _skipCount < array.Length)
+            {
+                var skipNumber = array.Skip(_skipCount);
+                foreach (int x in skipNumber)
+                    Debug.Log($"{x} ");
+            }
+            // Exercise 4. Take
+            if (_takeCount >= 0
+               && _takeCount < array.Length)
+            {
+                var takeNumber = array.Take(_takeCount);
+                foreach (int y in takeNumber)
+                    Debug.Log($"{y} ");
+            }
+        }
     }
 }
