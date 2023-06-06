79d9c3e68777dc0fc1295af28d54aa80ca1594bd
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2023-06-06 23:44:14 +0300

    fix: start, list

--- Assets/Scripts/CSharp7/CSharp7.cs
+++ Assets/Scripts/CSharp7/CSharp7.cs
@@ -7,34 +7,44 @@ public class CSharp7 : MonoBehaviour
     [SerializeField] private int _skipCount;
     [SerializeField] private int _takeCount;
 
+    private int _vectorCount = 5;
+    private List<int> xPositions;
+
+    public void Start()
+    {
+        var list = new List<Vector3>();
+        GetXPositions(list);
+    }
+
     public List<int> GetXPositions(List<Vector3> vectors)
     {
-        var xPositions = vectors
-           .Where(vector => vector.x > 5)
-           .Select(vector => (int)vector.x)
-           .ToList();
+        xPositions = vectors
+          .Where(vector => vector.x > _vectorCount)
+          .Select(vector => (int)vector.x)
+          .ToList();
+        GetArray();
         return xPositions;
+    }
 
-        void GetArray()
+    private void GetArray()
+    {
+        // Exercise 3.Skip, ToArray
+        var array = xPositions.ToArray();
+
+        if (_skipCount >= 0
+            && _skipCount < array.Length)
         {
-            // Exercise 3.Skip, ToArray
-            var array = xPositions.ToArray();
-
-            if (_skipCount >= 0
-                && _skipCount < array.Length)
-            {
-                var skipNumber = array.Skip(_skipCount);
-                foreach (int x in skipNumber)
-                    Debug.Log($"{x} ");
-            }
-            // Exercise 4. Take
-            if (_takeCount >= 0
-               && _takeCount < array.Length)
-            {
-                var takeNumber = array.Take(_takeCount);
-                foreach (int y in takeNumber)
-                    Debug.Log($"{y} ");
-            }
+            var skipNumber = array.Skip(_skipCount);
+            foreach (int x in skipNumber)
+                Debug.Log($"{x} ");
+        }
+        // Exercise 4. Take
+        if (_takeCount >= 0
+           && _takeCount < array.Length)
+        {
+            var takeNumber = array.Take(_takeCount);
+            foreach (int y in takeNumber)
+                Debug.Log($"{y} ");
         }
     }
 }
