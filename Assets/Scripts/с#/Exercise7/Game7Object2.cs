3040c7cc7bfb2c76d4d8c652049df6f2d0112a29
DariaDostoevskaia <122167406+DariaDostoevskaia@users.noreply.github.com>
2023-06-07 21:15:49 +0300

    fix: start obj gameobject7-2

--- Assets/Scripts/с#/Exercise7/Game7Object2.cs
+++ Assets/Scripts/с#/Exercise7/Game7Object2.cs
@@ -3,11 +3,15 @@ using UnityEngine;
 public class Game7Object2 : MonoBehaviour
 {
     [SerializeField] private Space5OnOff _space5OnOff;
+    private Space5OnOff obj;
+
+    private void Start()
+    {
+        obj = FindObjectOfType<Space5OnOff>();
+    }
 
     private void Update()
     {
-        var obj = FindObjectOfType<Space5OnOff>();
-
         if (Input.GetKeyDown(KeyCode.Return))
             obj.enabled = !obj.enabled;
     }
